313af4ec661b775f7c66b2475124e433af17306d
Chukhnov Max <4myself.max@gmail.com>
2020-08-29 13:01:44 +0300

    Create Cat class and some methods

---
+++ src/com/kp/chukhnovm/hw1_1/Cat.java
@@ -0,0 +1,78 @@
+package com.kp.chukhnovm.hw1_1;
+
+import java.util.Date;
+import java.util.Random;
+
+public class Cat {
+
+    private String name;
+    private String color;
+    private Date birthday;
+
+    public Cat(String name, Date birthday, String color) {
+        this.name = name;
+        this.birthday = birthday;
+        this.color = color;
+    }
+
+    /*
+    |--------------------------------------------------------------------------
+    | GETTERS & SETTERS
+    |--------------------------------------------------------------------------
+    */
+
+    public String getName() {
+        return name;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public Date getBirthday() {
+        return birthday;
+    }
+
+    /**
+     * Get age in days
+     *
+     * @return
+     */
+    public long getAgeInDays() {
+
+        Date d1 = this.getBirthday();
+        Date d2 = new Date();
+
+        return (d2.getTime() - d1.getTime()) / (24 * 60 * 60 * 1000);
+    }
+
+    /*
+    |--------------------------------------------------------------------------
+    | METHODS
+    |--------------------------------------------------------------------------
+    */
+
+    public void voice() {
+
+        String[] mood = {
+                "Purrs...",
+                "Uff",
+                "Meow",
+                "ЖРАТЬ ДАВАЙ!",
+        };
+
+        int rnd = new Random().nextInt(mood.length);
+
+        System.out.println(mood[rnd]);
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Cat {name='%s', color='%s', birthday='%3$tY-%3$tm-%3$td', age='%4$s'}",
+                this.getName(),
+                this.getColor(),
+                this.getBirthday(),
+                this.getAgeInDays()
+        );
+    }
+}
---
+++ src/com/kp/chukhnovm/hw1_1/Main.java
@@ -0,0 +1,21 @@
+package com.kp.chukhnovm.hw1_1;
+
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Cat[] cats = {
+            new Cat("Vasya", new GregorianCalendar(2017, Calendar.DECEMBER, 8).getTime(), "red"),
+            new Cat("Petya", new GregorianCalendar(2016, Calendar.JULY, 21).getTime(), "green"),
+            new Cat("Kisa", new GregorianCalendar(2019, Calendar.JULY, 5).getTime(), "yellow")
+        };
+
+        for (Cat cat : cats) {
+            System.out.println(cat);
+            cat.voice();
+        }
+    }
+}
